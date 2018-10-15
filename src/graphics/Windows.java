bf63aa5529a043335af5afea5fb8804db1cbb911
Personface <Personface@141.7.148.115>
2018-10-15 11:58:04 +0200

    add a simple frame that doesn't do anything yet

--- src/graphics/Windows.java
+++ src/graphics/Windows.java
@@ -1,6 +1,46 @@
 package graphics;
 
-public class Windows
-{
+import java.awt.Button;
+import java.awt.CardLayout;
 
+import javax.swing.JFrame;
+
+public class Windows extends JFrame
+{
+	
+	/*
+	private Button death = new Button("death");
+	private Button life = new Button("life");
+	private Button useless = new Button("-_-");
+	*/
+	private Button something = new Button("something");
+	private CardLayout myLayout = new CardLayout();
+	
+	public Windows()
+	{
+		this.setTitle("Some title");
+		this.setSize(384,216);
+		/*
+		this.setLayout(myLayout);
+		this.add(death);
+		this.add(life);
+		this.add(useless);
+		*/
+		/*
+		this.add(death, BorderLayout.EAST);
+		this.add(life, BorderLayout.WEST);
+		this.add(useless, BorderLayout.CENTER);
+		*/
+		this.add(something);
+		this.setDefaultCloseOperation(Windows.EXIT_ON_CLOSE);
+		
+		/*
+		ActionListener dListen = new DeathListener();
+		death.addActionListener(dListen);
+		ActionListener lListen = new LifeListener();
+		life.addActionListener(lListen);
+		*/
+
+		this.setVisible(true);
+	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,17 @@
 package main;
 
 import java.awt.Color;
+import java.awt.Frame;
 
 import rectMaze.Maze;
 import graphics.Graphics;
+import graphics.Windows;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Frame f = new Windows();
+		
 		Maze maze = Maze.depthFirstGen(20, 20, true);
 		//maze.display();
 		Graphics.export(maze, "D:\\Data\\WinFolders\\Desktop\\pic.png", 10, Color.decode("#ff90e3"), Color.decode("#90ffc6"));
