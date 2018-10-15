54e2d8650efd8b9771822a988b06e6e7345bf099
Personface <Personface@141.7.148.115>
2018-10-15 12:55:30 +0200

    Nearly ready, all the things are in the frame but aren't listened/read

--- src/graphics/RectMazeDialogue.java
+++ src/graphics/RectMazeDialogue.java
@@ -1,21 +1,44 @@
 package graphics;
 
 import java.awt.Button;
-import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import java.text.NumberFormat;
 
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 
 public class RectMazeDialogue extends JFrame
 {
-	private Button something = new Button("something");
-	private CardLayout myLayout = new CardLayout();
+	private JFormattedTextField widthField = new JFormattedTextField(NumberFormat.getNumberInstance());
+	private JFormattedTextField heightField = new JFormattedTextField(NumberFormat.getNumberInstance());
+	private JFormattedTextField backgroundColorField = new JFormattedTextField("000000");
+	private JFormattedTextField foregroundColorField = new JFormattedTextField("FFFFFF");
+	private Button send = new Button("enter");
+	
+	private static final int HORIZONTAL_GAP = 10;
+    private static final int VERTICAL_GAP = 10;
+	private static FlowLayout flow = new FlowLayout(FlowLayout.CENTER, HORIZONTAL_GAP, VERTICAL_GAP);
 	
 	public RectMazeDialogue()
 	{
 		this.setTitle("Some title");
 		this.setSize(384,216);
+		this.setLayout(flow);
 
-		this.add(something);
+		this.add(widthField);
+		widthField.setValue(10);
+		widthField.setColumns(5);
+		this.add(heightField);
+		heightField.setValue(10);
+		heightField.setColumns(5);
+		
+		this.add(backgroundColorField);
+		backgroundColorField.setColumns(5);
+		this.add(foregroundColorField);
+		foregroundColorField.setColumns(5);
+		
+		this.add(send);
+		
 		this.setDefaultCloseOperation(Windows.EXIT_ON_CLOSE);
 		
 		this.setVisible(true);
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,16 @@
 package main;
 
-import java.awt.Color;
 import java.awt.Frame;
 
-import rectMaze.Maze;
-import graphics.Graphics;
-import graphics.Windows;
+import graphics.RectMazeDialogue;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Frame f = new Windows();
-		
+		Frame f = new RectMazeDialogue();
+		/*
 		Maze maze = Maze.depthFirstGen(20, 20, true);
-
 		Graphics.export(maze, "D:\\Data\\WinFolders\\Desktop\\pic.png", 10, Color.decode("#ff90e3"), Color.decode("#90ffc6"));
+		*/
 	}
 }
