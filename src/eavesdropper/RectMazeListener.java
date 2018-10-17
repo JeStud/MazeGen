8d55638a4da971fd0fe0fa69a706040b41fcd7c1
Personface <Personface@141.7.148.115>
2018-10-17 12:26:24 +0200

    Frame now exports picture

--- src/eavesdropper/RectMazeListener.java
+++ src/eavesdropper/RectMazeListener.java
@@ -1,15 +1,67 @@
 package eavesdropper;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.NumberFormat;
+
+import javax.swing.JFormattedTextField;
+
+import graphics.Graphics;
+import rectMaze.Maze;
 
 public class RectMazeListener implements ActionListener
 {
-
+	private JFormattedTextField widthField = new JFormattedTextField(NumberFormat.getNumberInstance());
+	private JFormattedTextField heightField = new JFormattedTextField(NumberFormat.getNumberInstance());
+	private JFormattedTextField backgroundColorField = new JFormattedTextField("000000"); //TODO needs the # added on to the front
+	private JFormattedTextField foregroundColorField = new JFormattedTextField("FFFFFF"); //TODO needs the # added on to the front
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		int x, y;
+		Color bkClr, frClr;
+		//get field values and generate maze
+		//check if the width/height is an int and save as x and y
+		try
+			{ x = Integer.parseInt( widthField.getText() ); }
+		catch(NumberFormatException exceptX)
+			{ x = 1;/*TODO display an error pop up*/ }
+		try
+			{ y = Integer.parseInt( heightField.getText() ); }
+		catch(NumberFormatException exceptY)
+			{ y = 1;/*TODO display an error pop up*/ }
+		//check if the fore/background are Colors and save as bkClr and frClr
+		try
+			{ bkClr = Color.decode("#" + backgroundColorField.getText() ); }
+		catch(NumberFormatException exceptBK)
+			{ bkClr = Color.BLUE;/*TODO display an error pop up*/ }
+		try
+			{ frClr = Color.decode("#" + foregroundColorField.getText() ); }
+		catch(NumberFormatException exceptFR)
+			{ frClr = Color.RED;/*TODO display an error pop up*/ }
 		
+		Maze maze = Maze.depthFirstGen(y, x, true);
+		Graphics.export(maze, "D:\\Data\\WinFolders\\Desktop\\pic.png", 10, bkClr, frClr);
+		
+		//call a function that opens a window with the maze
+		//call a function that opens a window with an export option and then returns
 	}
 	
+	public JFormattedTextField getWidthField()
+	{
+		return this.widthField;
+	}
+	public JFormattedTextField getHeightField()
+	{
+		return this.heightField;
+	}
+	public JFormattedTextField getBackgroundColorField()
+	{
+		return this.backgroundColorField;
+	}
+	public JFormattedTextField getForegroundColorField()
+	{
+		return this.foregroundColorField;
+	}
 }
--- src/graphics/RectMazeDialogue.java
+++ src/graphics/RectMazeDialogue.java
@@ -2,25 +2,31 @@ package graphics;
 
 import java.awt.Button;
 import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
 import java.text.NumberFormat;
 
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 
+import eavesdropper.RectMazeListener;
+
 public class RectMazeDialogue extends JFrame
 {
-	private JFormattedTextField widthField = new JFormattedTextField(NumberFormat.getNumberInstance());
-	private JFormattedTextField heightField = new JFormattedTextField(NumberFormat.getNumberInstance());
-	private JFormattedTextField backgroundColorField = new JFormattedTextField("000000");
-	private JFormattedTextField foregroundColorField = new JFormattedTextField("FFFFFF");
 	private Button send = new Button("enter");
 	
 	private static final int HORIZONTAL_GAP = 10;
     private static final int VERTICAL_GAP = 10;
 	private static FlowLayout flow = new FlowLayout(FlowLayout.CENTER, HORIZONTAL_GAP, VERTICAL_GAP);
 	
+	private RectMazeListener rectMazeListener = new RectMazeListener();
+	
 	public RectMazeDialogue()
 	{
+		JFormattedTextField widthField = rectMazeListener.getWidthField();
+		JFormattedTextField heightField = rectMazeListener.getHeightField();
+		JFormattedTextField backgroundColorField = rectMazeListener.getBackgroundColorField();
+		JFormattedTextField foregroundColorField = rectMazeListener.getForegroundColorField();
+		
 		this.setTitle("Some title");
 		this.setSize(384,216);
 		this.setLayout(flow);
@@ -38,6 +44,7 @@ public class RectMazeDialogue extends JFrame
 		foregroundColorField.setColumns(5);
 		
 		this.add(send);
+		send.addActionListener( rectMazeListener );
 		
 		this.setDefaultCloseOperation(Windows.EXIT_ON_CLOSE);
 		
